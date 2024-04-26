baaf3c5435a71e3bc54833b54bf24bd9134b6a43
심정민 <gkstlago123@naver.com>
2024-04-26 17:20:36 +0900

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import BasicButtons from './components/BasicButtons';
+
 function App() {
-  return
-      <div>
-      <BasicButtons/>
-      </div>
+  return;
+  <div>
+    <BasicButtons />
+  </div>;
 }
 
-
-export default App
+export default App;
--- src/components/BasicButtons/index.tsx
+++ src/components/BasicButtons/index.tsx
@@ -1,16 +1,12 @@
-import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
 export default function BasicButtons() {
-    const [num, setNum] = useState(0);
-
-
   return (
     <Stack spacing={2} direction="row">
-        <div>{num}</div>
-      <Button variant="text">Increase</Button>
-      <Button variant="contained">decrease</Button>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </Stack>
   );
 }
